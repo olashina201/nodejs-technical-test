08611012652ce325614f26d187c5ed150dad2ae2
olashina201 <olashina201@gmail.com>
2023-08-18 21:19:24 +0100

    removed unused files

--- src/interfaces/api-error.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IResponseStatusCodes } from "./response.interface";
-
-export interface IApiErrorArguments {
-  message: string;
-  name?: string;
-  statusCode: IResponseStatusCodes;
-  isOperational?: boolean;
-}
